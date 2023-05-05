9da3554ce738a03f2b86e9b2babb63067bda022f
harisishtiaq1 <harisharry232@gmail.com>
2023-05-05 14:56:24 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import './App.css';
-import { Auth } from './components/Auth';
-
+import "./App.css";
+import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+import { useState, useRef } from "react";
+import { Box, Button, Stack, Typography } from "@mui/material";
+const cookies = new Cookies();
 function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+  const roomInputRef = useRef(null);
+  console.log(isAuth, "Auth");
+  console.log(room, "room");
+  console.log(roomInputRef, "roomInputRef");
+
+  if (!isAuth) {
+    return (
+      <>
+        <Auth setIsAuth={setIsAuth} />
+      </>
+    );
+  }
   return (
-    <div>
-      <Auth/>
-    </div>
+    <>
+      {room ? (
+        <Typography sx={{ fontWeight: "500" }}> Chat </Typography>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Typography variant="h4" component="h4" sx={{ fontWeight: "500" }}>
+            Enter Room Name
+          </Typography>
+          <Stack
+            spacing={4}
+            direction="column"
+            width={200}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <input
+              variant="outlined"
+              label="Enter Room Name"
+              placeholder="Enter Room Name"
+              style={{ mt: 4 }}
+              ref={roomInputRef}
+            />
+            <Button
+              variant="contained"
+              onClick={() => setRoom(roomInputRef.current.value)}
+            >
+              Enter Chat
+            </Button>
+          </Stack>
+        </Box>
+      )}
+    </>
   );
 }
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,25 +1,46 @@
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
-export const Auth = () => {
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+    fontWeightBold: "600",
+  },
+  backgroundColor: "#F4F7FE",
+});
+export const Auth = (props) => {
+  const { setisAuth } = props;
   const signInWithGoogle = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
       cookies.set("auth-token", result.user.refreshToken);
+      setisAuth(true);
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <Box sx={{ textAlign: "center", mt: 8,fontFamily:'Poppins' }}>
-      <Typography variant="h4" component="h4" sx={{ m: 4, fontWeight: "500"}}>
-        Sign in with Google to Continue
-      </Typography>
-      <Button onClick={signInWithGoogle} variant="contained">
-        Sign in with google
-      </Button>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ textAlign: "center", mt: 8, fontFamily: "Poppins" }}>
+        <Typography
+          variant="h4"
+          component="h4"
+          sx={{ m: 4, fontWeight: "500" }}
+        >
+          Sign in with Google to Continue
+        </Typography>
+        <Button onClick={signInWithGoogle} variant="contained">
+          <Typography>Sign in with google</Typography>
+        </Button>
+      </Box>
+    </ThemeProvider>
   );
 };
