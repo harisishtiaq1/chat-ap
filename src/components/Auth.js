effbafae200cefc748d69f345f4c49a7caab0a75
harisishtiaq1 <harisharry232@gmail.com>
2023-05-05 11:49:33 +0500

    Updated

---
+++ src/components/Auth.js
@@ -0,0 +1,25 @@
+import { Box, Button, Typography } from "@mui/material";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+export const Auth = () => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <Box sx={{ textAlign: "center", mt: 8 }}>
+      <Typography variant="h4" component="h4" sx={{ m: 4, fontWeight: "500",fontStyle:'italic' }}>
+        Sign in with Google to Continue
+      </Typography>
+      <Button onClick={signInWithGoogle} variant="contained">
+        Sign in with google
+      </Button>
+    </Box>
+  );
+};
